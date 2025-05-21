fd988344f1d6b97f31d967a2de4df8694450acb7
AnLaxina <anilovlaxina@gmail.com>
2025-05-20 22:46:11 -0700

    Add weatherManager class
    All this does right now is just test to see if imports are working

--- code/index.js
+++ code/index.js
@@ -1,1 +1,2 @@
-console.log("Hello! I run when the page loads!");
+import WeatherManager from "./weatherManager.js";
+WeatherManager.initialize();
---
+++ code/weatherManager.js
@@ -0,0 +1,7 @@
+export default class WeatherManager {
+    // API Key: NJ5X7VJZ62AWQLWL5WCRJK7GV
+    static initialize() {
+        // For now, let's just test out the API and see if everything is working.
+        console.log("Hello! I'm in the weather manager class");
+    }
+}
