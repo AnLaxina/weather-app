015c2ae7963dbd922ceeb8ef8c77a24f10d8ab08
AnLaxina <anilovlaxina@gmail.com>
2025-05-27 22:04:14 -0700

    Add functionality for loading dynamic icons

--- code/domManager.js
+++ code/domManager.js
@@ -1,7 +1,14 @@
 import WeatherManager from "./weatherManager.js";
 
 export default class DOMManager {
-    static initialize() {
+    static async initialize() {
+        await this.#changeWeatherIcon();
         console.log("The DOM Manager has initialized!");
+    }
+
+    static async #changeWeatherIcon() {
+        const img = document.getElementById("weather-icon");
+        const currentIcon = await WeatherManager.getCurrentConditions().icon;
+        img.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/${currentIcon}.png`;
     }
 }
--- code/index.js
+++ code/index.js
@@ -1,8 +1,8 @@
 import WeatherManager from "./weatherManager.js";
 import DOMManager from "./domManager.js";
 
-DOMManager.initialize();
 await WeatherManager.initialize("Maple Ridge, BC");
+await DOMManager.initialize();
 
 WeatherManager.logWeatherData();
 console.log(`The weather icon is ${WeatherManager.getCurrentConditions().icon}`)
