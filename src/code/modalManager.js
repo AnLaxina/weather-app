c0694a353e612b2e8f2fc16986b7bc8bd49096b1
AnLaxina <anilovlaxina@gmail.com>
2025-06-04 11:53:41 -0700

    Add a basic input validation that prints out a custom message if it's invalid
    For now, it DOES not check if it's a valid location/country

--- src/code/modalManager.js
+++ src/code/modalManager.js
@@ -1,6 +1,30 @@
 export default class ModalManager {
+    static modal;
+
     static initialize() {
-        const modal = document.querySelector("#location-popup");
-        modal.showModal();
+        this.modal = document.querySelector("#location-popup");
+        this.modal.showModal();
+
+        this.#addEventListenersDone();
+    }
+
+    static #addEventListenersDone() {
+        const doneButton = document.getElementById("done-button");
+        doneButton.addEventListener("click", () => {
+            if (this.#checkValidInput()) {
+                this.modal.close();
+            }
+        })
+    }
+
+    static #checkValidInput() {
+        const textInput = document.querySelector("input");
+        const validityState = textInput.validity;
+        if(validityState.valueMissing) {
+            textInput.setCustomValidity("Enter a valid location please!");
+            textInput.reportValidity();
+            return false;
+        }
+        return true;
     }
 }
